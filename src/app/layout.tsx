a7b78c1bdb7debc88fbec4eb56bfdd22479b0e96
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-06-17 06:14:17 +0300

    init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './globals.css';
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
