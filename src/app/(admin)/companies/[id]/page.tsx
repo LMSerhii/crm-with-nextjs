f4f69b651d52123e0f3b77c04119ea757d38070e
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-06-30 20:35:06 +0300

    intercepting routes

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -9,16 +9,9 @@ export interface PageProps {
 }
 
 export default function Page({ params }: PageProps) {
-  useEffect(() => {
-    const id = Number.parseInt(params.id);
-
-    if (Number.isNaN(id)) {
-      notFound();
-    }
-  }, [params.id]);
   return (
-    <>
-      <Header>Companies: ({params.id})</Header>
-    </>
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
   );
 }
--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import Header from '../../components/header';
-import Toolbar from '../../components/toolbar';
-import SearchInput from '../../components/search-input';
-import AddCompanyButton from '../../components/add-company-button';
 import CompanyTable from '../../components/company-table';
+import CompanyRow from '@/app/components/company-row';
+import { CompanyStatus } from '@/lib/api';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
-    </>
+    <CompanyTable>
+      <CompanyRow company="Costco" />
+    </CompanyTable>
   );
 }
